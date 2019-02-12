ffd926fe61012313676b7bf94bdb236d2470e09d
Ali Azin <ali_azin66@yahoo.com>
2019-02-12 09:48:09 +0330

    first day

---
+++ Assets/Scripts/MortarControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MortarControl : MonoBehaviour
+{
+    public Transform CannonBarrel;
+
+    private float _angle = 0f;
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            _angle += 5;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            _angle -=  5;
+        }
+
+        CannonBarrel.eulerAngles = new Vector3(0, 0, _angle);
+
+
+    }
+
+
+}
