a6e1a1c85e6fa03a34ad76fe18b945f483a7bc09
Ali Azin <ali_azin66@yahoo.com>
2019-02-13 18:03:13 +0330

    day 4

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadLevel : MonoBehaviour
+{
+    public string LevelName;
+    public void LoadLevelWithName()
+    {
+        SceneManager.LoadScene(LevelName);
+    }
+}
